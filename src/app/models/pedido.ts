d0c1e8d5fd1c67b77c2fe3525ab8d86e1724871c
Rafael Ch <rafael.cherman0@gmail.com>
2023-10-19 19:54:01 -0300

    adicionado comentarios

--- src/app/models/pedido.ts
+++ src/app/models/pedido.ts
@@ -3,10 +3,15 @@ import { Produtodiverso } from "./produtodiverso";
 
 export class Pedido {
 
+    //cliente
+    //atendente
+    //endereco
+    //entregador
     solicitaEntrega!: boolean;
     situacaoPedido!: string;
     valorTotal!: number;
     formaPagamento!: string;
+    //data
     pizzas!: Pizza[];
     produtos!: Produtodiverso[];
 }
