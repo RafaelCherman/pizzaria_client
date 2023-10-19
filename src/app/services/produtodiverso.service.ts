f501382034f1cab6d953ba8b10c9d57f5f1797a4
Rafael Ch <rafael.cherman0@gmail.com>
2023-10-19 19:54:22 -0300

    feito alguns metodos na service

--- src/app/services/produtodiverso.service.ts
+++ src/app/services/produtodiverso.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Produtodiverso } from '../models/produtodiverso';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutodiversoService {
 
+  API: string = 'http://localhost:8080/api/produto_diverso';
+  http = inject(HttpClient);
+
   constructor() { }
+
+  findById(id: number): Observable<Produtodiverso>{
+    let params = new HttpParams()
+      .set('id', id)
+    
+    return this.http.get<Produtodiverso>(this.API, {params: params});
+  }
+
+  listAll(): Observable<Produtodiverso[]> {
+    return this.http.get<Produtodiverso[]>(this.API + "/all");
+  }
+
+  save(produto: Produtodiverso): Observable<string> {
+    return this.http.post<string>(this.API, produto);
+  }
+
+  edit(id: number, produto: Produtodiverso): Observable<string>{
+    let params = new HttpParams()
+      .set('id', id)
+
+      return this.http.put<string>(this.API, produto, { params: params});
+  }
+
+  delete(id: number): Observable<any>{
+    let params = new HttpParams()
+      .set('id', id)
+    
+    return this.http.delete<any>(this.API, {params: params});
+  }
 }
