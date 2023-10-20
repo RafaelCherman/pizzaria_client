813caea58bb76309098281d6886e4d64ca3e6510
Rafael Ch <rafael.cherman0@gmail.com>
2023-10-19 21:48:24 -0300

    testando criar um pedido

--- src/app/components/pedido/pedidoselecionarpizza/pedidoselecionarpizza.component.ts
+++ src/app/components/pedido/pedidoselecionarpizza/pedidoselecionarpizza.component.ts
@@ -1,5 +1,10 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { Pizza } from 'src/app/models/pizza';
+import { Pizzatipo } from 'src/app/models/pizzatipo';
+import { Sabor } from 'src/app/models/sabor';
+import { PizzaService } from 'src/app/services/pizza.service';
+import { PizzatipoService } from 'src/app/services/pizzatipo.service';
+import { SaborService } from 'src/app/services/sabor.service';
 
 @Component({
   selector: 'app-pedidoselecionarpizza',
@@ -12,14 +17,65 @@ export class PedidoselecionarpizzaComponent {
   pizza: Pizza = new Pizza();
   valorPizza: number = 0;
 
+
+  pizzaService = inject(PizzaService);
+
+
+
+  //PARA TESTES
+  sabores: Sabor[] = [];
+  pizzaTipo: Pizzatipo = new Pizzatipo();
+  saborService = inject(SaborService);
+  pizzaTipoService = inject(PizzatipoService);
+  //ALTERAR RETORNOS DO BACKEND
+
   constructor()
   {
-    this.pizza.valorPizza = 50;
+    this.listAllSabores();
+    this.findTipoById();
+  }
+
+  listAllSabores(){
+    this.saborService.listAll().subscribe({
+      next: lista =>{
+        this.sabores = lista;
+      },
+      error: erro =>{
+        alert('Exemplo de tratamento de erro/exception! Observe o erro no console!');
+        console.error(erro);
+      }
+    })
+  }
+  
+  findTipoById()
+  {
+    this.pizzaTipoService.findById(1).subscribe({
+      next: tipo =>{
+        this.pizzaTipo = tipo;
+      },
+      error: erro =>{
+        alert('Exemplo de tratamento de erro/exception! Observe o erro no console!');
+        console.error(erro);
+      }
+    })
   }
 
   adicionar()
   {
-    this.retorno.emit(this.pizza);
+    this.pizza.tipo = this.pizzaTipo;
+    this.pizza.sabor = this.sabores;
+
+    this.pizzaService.save(this.pizza).subscribe({
+      next: sucesso =>{
+        this.retorno.emit(this.pizza);
+        
+      },
+      error: erro =>{
+        alert('Exemplo de tratamento de erro/exception! Observe o erro no console!');
+        console.error(erro);
+        console.log(this.pizza);
+      }
+    })
   }
 
 }
