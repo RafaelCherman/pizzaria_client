656054689ae7c3c44be889229eb12efe12bf5fe1
Rafael Ch <rafael.cherman0@gmail.com>
2023-10-22 03:18:49 -0300

    feito services

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../models/cliente';
+import { Resposta } from '../models/resposta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
+  API: string = 'http://localhost:8080/api/cliente';
+  http = inject(HttpClient);
 
   constructor() { }
+
+  findById(id: number): Observable<Cliente>{
+    let params = new HttpParams()
+      .set('id', id)
+    
+    return this.http.get<Cliente>(this.API, {params: params});
+  }
+
+  listAll(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.API + "/all");
+  }
+
+  save(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.API, cliente);
+  }
+
+  edit(id: number, cliente: Cliente): Observable<Cliente>{
+    let params = new HttpParams()
+      .set('id', id)
+
+      return this.http.put<Cliente>(this.API, cliente, { params: params});
+  }
+
+  delete(id: number): Observable<Resposta>{
+    let params = new HttpParams()
+      .set('id', id)
+    
+    return this.http.delete<Resposta>(this.API, {params: params});
+  }
+
 }
--- src/app/services/endereco.service.ts
+++ src/app/services/endereco.service.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Endereco } from '../models/endereco';
+import { end } from '@popperjs/core';
+import { Resposta } from '../models/resposta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnderecoService {
 
+  
+  API: string = 'http://localhost:8080/api/endereco';
+  http = inject(HttpClient);
+
   constructor() { }
+
+  findById(id: number): Observable<Endereco>{
+    let params = new HttpParams()
+      .set('id', id)
+    
+    return this.http.get<Endereco>(this.API, {params: params});
+  }
+
+  listAll(): Observable<Endereco[]> {
+    return this.http.get<Endereco[]>(this.API + "/all");
+  }
+
+  save(endereco: Endereco): Observable<Endereco> {
+    return this.http.post<Endereco>(this.API, endereco);
+  }
+
+  edit(id: number, endereco: Endereco): Observable<Endereco>{
+    let params = new HttpParams()
+      .set('id', id)
+
+      return this.http.put<Endereco>(this.API, endereco, { params: params});
+  }
+
+  delete(id: number): Observable<Resposta>{
+    let params = new HttpParams()
+      .set('id', id)
+    
+    return this.http.delete<Resposta>(this.API, {params: params});
+  }
+
 }
