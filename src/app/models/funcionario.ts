b01caf53a7a01fd9d20238be8fc81b5d68dbabec
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-11-29 14:24:56 -0300

    feito teste de endereco, cliente

--- src/app/models/funcionario.ts
+++ src/app/models/funcionario.ts
@@ -4,3 +4,19 @@ export class Funcionario {
   cpf!: String;
   funcao!: string;
 }
+
+/*
+export class Funcionario {
+  id: number;
+  nome: string;
+  cpf: string;
+  funcao: string;
+
+  constructor(id: number, nome: string, cpf: string, funcao: string) {
+    this.id = id;
+    this.nome = nome;
+    this.cpf = cpf;
+    this.funcao = funcao;
+  }
+}
+*/
