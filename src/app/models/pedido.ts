208173702c6e131cc5994b4c08df8adbc4354645
Rafael Ch <rafael.cherman0@gmail.com>
2023-12-01 18:11:52 -0300

    resolvendo bug

--- src/app/models/pedido.ts
+++ src/app/models/pedido.ts
@@ -1,4 +1,4 @@
-/*import { Cliente } from "./cliente";
+import { Cliente } from "./cliente";
 import { Endereco } from "./endereco";
 import { Funcionario } from "./funcionario";
 import { Pizza } from "./pizza";
@@ -18,54 +18,4 @@ export class Pedido {
     dataPedido!: string;
     pizzas!: Pizza[];
     produtos!: Produtodiverso[];
-}
-*/
-// pedido.ts
-import { Cliente } from './cliente';
-import { Endereco } from './endereco';
-import { Funcionario } from './funcionario';
-import { Pizza } from './pizza';
-import { Produtodiverso } from './produtodiverso';
-
-export class Pedido {
-    id!: number;
-    cliente!: Cliente;
-    atendente!: Funcionario;
-    endereco!: Endereco;
-    entregador!: Funcionario;
-    solicitaEntrega!: boolean;
-    situacaoPedido!: string;
-    valorTotal!: number;
-    formaPagamento!: string;
-    dataPedido!: string;
-    pizzas!: Pizza[];
-    produtos!: Produtodiverso[];
-
-    constructor(
-        id: number = 0,
-        cliente: Cliente = new Cliente(),
-        atendente: Funcionario = new Funcionario(),
-        endereco: Endereco = new Endereco(),
-        entregador: Funcionario = new Funcionario(),
-        solicitaEntrega: boolean = false,
-        situacaoPedido: string = '',
-        valorTotal: number = 0,
-        formaPagamento: string = '',
-        dataPedido: string = '',
-        pizzas: Pizza[] = [],
-        produtos: Produtodiverso[] = []
-    ) {
-        this.id = id;
-        this.cliente = cliente;
-        this.atendente = atendente;
-        this.endereco = endereco;
-        this.entregador = entregador;
-        this.solicitaEntrega = solicitaEntrega;
-        this.situacaoPedido = situacaoPedido;
-        this.valorTotal = valorTotal;
-        this.formaPagamento = formaPagamento;
-        this.dataPedido = dataPedido;
-        this.pizzas = pizzas;
-        this.produtos = produtos;
-    }
 }
